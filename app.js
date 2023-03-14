2c0ca1b91fcec37c6ad321ede3e2726393376f1e
Joshua Chan <joshua@appdevdesigns.net>
2023-03-13 22:18:04 -0700

    Use spaces for indentation

--- app.js
+++ app.js
@@ -12,28 +12,28 @@ var controller = AB.controller("file_processor");
 controller.init();
 
 if (process.env.CLAMAV_ENABLED == "true") {
-	child_process.execFile(
-		"chown",
-		[ "-R", "clamav:clamav", "/var/lib/clamav" ],
-		(err, stdout, stderr) => {
-			if (err) {
-				console.error("Could not initialize clamav volume.")
-				console.error(stderr);
-				console.error(err);
-			} else {
-				// Automatically refresh virus definition DB throughout the day
-				child_process.execFile("freshclam", [ "-d", "--daemon-notify" ]);
-				// Load the ClamAV daemon
-				child_process.execFile("clamd", (err, stdout, stderr) => {
-					if (err) {
-						console.error("Could not start ClamAV daemon.")
-						console.error(stderr);
-						console.error(err);
-					} else {
-						console.log("ClamAV daemon ready");
-					}
-				});
-			}
-		}
-	);
+   child_process.execFile(
+      "chown",
+      ["-R", "clamav:clamav", "/var/lib/clamav"],
+      (err, stdout, stderr) => {
+         if (err) {
+            console.error("Could not initialize clamav volume.");
+            console.error(stderr);
+            console.error(err);
+         } else {
+            // Automatically refresh virus definition DB throughout the day
+            child_process.execFile("freshclam", ["-d", "--daemon-notify"]);
+            // Load the ClamAV daemon
+            child_process.execFile("clamd", (err, stdout, stderr) => {
+               if (err) {
+                  console.error("Could not start ClamAV daemon.");
+                  console.error(stderr);
+                  console.error(err);
+               } else {
+                  console.log("ClamAV daemon ready");
+               }
+            });
+         }
+      }
+   );
 }
