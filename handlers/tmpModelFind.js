a423331627c076a530510645c3fa09424a514b8b
nohrsom <sky_wyvern@hotmail.com>
2023-05-01 21:34:50 +0700

    Fix dir name and prettier

--- handlers/tmpModelFind.js
+++ handlers/tmpModelFind.js
@@ -9,47 +9,45 @@
 // const Model = require(path.join(__dirname, "model"));
 
 module.exports = {
-    /**
-     * Key: the cote message key we respond to.
-     */
-    key: "model.find",
+   /**
+    * Key: the cote message key we respond to.
+    */
+   key: "model.find",
 
-    /**
-     * fn
-     * our Request handler.
-     * @param {obj} req
-     *        the request object sent by the apiSails controller.
-     * @param {fn} cb
-     *        a node style callback(err, results) to send data when job is finished
-     */
-    fn: function handler(req, cb) {
-        var err;
+   /**
+    * fn
+    * our Request handler.
+    * @param {obj} req
+    *        the request object sent by the apiSails controller.
+    * @param {fn} cb
+    *        a node style callback(err, results) to send data when job is finished
+    */
+   fn: function handler(req, cb) {
+      var err;
 
-        // if config not set, we have not be initialized properly.
-        var config = req.config();
-        if (!config) {
-            req.log(`WARN: ${this.key} handler not setup properly.`);
-            err = new Error(`${this.key}: Missing config`);
-            err.code = "EMISSINGCONFIG";
-            err.req = req;
-            cb(err);
-            return;
-        }
+      // if config not set, we have not be initialized properly.
+      var config = req.config();
+      if (!config) {
+         req.log(`WARN: ${this.key} handler not setup properly.`);
+         err = new Error(`${this.key}: Missing config`);
+         err.code = "EMISSINGCONFIG";
+         err.req = req;
+         cb(err);
+         return;
+      }
 
-        // check if we are enabled
-        if (!config.enable) {
-            // we shouldn't be getting notification.email messages
-            req.log(
-                `WARN: ${this.key} job received, but config.enable is false.`
-            );
-            err = new Error(`${this.key} service is disabled.`);
-            err.code = "EDISABLED";
-            cb(err);
-            return;
-        }
+      // check if we are enabled
+      if (!config.enable) {
+         // we shouldn't be getting notification.email messages
+         req.log(`WARN: ${this.key} job received, but config.enable is false.`);
+         err = new Error(`${this.key} service is disabled.`);
+         err.code = "EDISABLED";
+         cb(err);
+         return;
+      }
 
-        // verify required parameters in job
-        /*
+      // verify required parameters in job
+      /*
             if (!req.email) {
               var err2 = new Error(
                 ".email parameter required in file.upload service."
@@ -60,8 +58,8 @@ module.exports = {
             }
          */
 
-        cb(null, { found: "it" });
+      cb(null, { found: "it" });
 
-        // cb(null, { uuid: "123456" });
-    }
+      // cb(null, { uuid: "123456" });
+   },
 };
